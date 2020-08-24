487553379cec75cb27c0ad05b4147bbe6f47a227
xiaoxie666 <843606180@qq.com>
2020-08-24 21:29:40 +0800

    项目构建 路由 element ui

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="login">
+        这是登录页面666
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Login"
+}
+</script>
+
+<style lang="scss" scoped>
+#login{
+    background-color:#344a5f;
+    height: 100vh; 
+}
+</style>
